26b50829f9d1f0a749edd01614087fc8745ab8db
zira <ziraproton@proton.me>
2026-02-24 16:53:05 -0500

    Optimized hello_world.cpp with std::

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,6 @@
 #include <iostream> // Input/Output stream
-using namespace std; // Uses the std namespace (standard? I think)
 
 int main() { // Main function
-    cout << "Hello, World!"; // C output stream std which outputs "Hello, World!"
+    std::cout << "Hello, World!"; // C output stream std which outputs "Hello, World!"
     return 0; // Returns 0 which is exit success
 }
